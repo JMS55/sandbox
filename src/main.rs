a0f691ecf19b22a533eb83c151b6bc8e3cb96207
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-05-04 12:07:10 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,158 @@
+mod sandbox;
+
+use pixels::{wgpu::Surface, Pixels, SurfaceTexture};
+use sandbox::{Particle, ParticleType, Sandbox};
+use std::time::{Duration, Instant};
+use winit::dpi::{LogicalSize, PhysicalPosition};
+use winit::event::{ElementState, Event, MouseButton, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
+pub const SIMULATION_WIDTH: usize = 600;
+pub const SIMULATION_HEIGHT: usize = 400;
+const DISPLAY_WIDTH: u32 = (SIMULATION_WIDTH * 2) as u32;
+const DISPLAY_HEIGHT: u32 = (SIMULATION_HEIGHT * 2) as u32;
+const TARGET_TIME_PER_UPDATE: Duration = Duration::from_nanos(16666670);
+
+fn main() {
+    let mut sandbox = Sandbox::new();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Sandbox")
+        .with_inner_size(LogicalSize::new(
+            DISPLAY_WIDTH as f64,
+            DISPLAY_HEIGHT as f64,
+        ))
+        .build(&event_loop)
+        .unwrap();
+
+    let surface = Surface::create(&window);
+    let surface_texture = SurfaceTexture::new(DISPLAY_WIDTH, DISPLAY_HEIGHT, surface);
+    let mut pixels = Pixels::new(
+        SIMULATION_WIDTH as u32,
+        SIMULATION_HEIGHT as u32,
+        surface_texture,
+    )
+    .unwrap();
+
+    let mut last_update = Instant::now() - TARGET_TIME_PER_UPDATE;
+    let mut paused = false;
+
+    let mut selected_particle = None;
+    let mut brush_size = 3;
+    let mut should_place_particles = false;
+
+    let mut prev_cursor_position = PhysicalPosition::<f64>::new(0.0, 0.0);
+    let mut curr_cursor_position = PhysicalPosition::<f64>::new(0.0, 0.0);
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::CursorMoved { position, .. } => {
+                    prev_cursor_position = curr_cursor_position;
+                    curr_cursor_position = position;
+                }
+                WindowEvent::MouseInput { button, state, .. } => {
+                    if button == MouseButton::Left {
+                        should_place_particles = state == ElementState::Pressed;
+                    }
+                }
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if input.state == ElementState::Pressed {
+                        match input.virtual_keycode {
+                            Some(VirtualKeyCode::Escape) => *control_flow = ControlFlow::Exit,
+                            Some(VirtualKeyCode::P) => paused = !paused,
+                            Some(VirtualKeyCode::Equals) => {
+                                if brush_size < 10 {
+                                    brush_size += 1
+                                }
+                            }
+                            Some(VirtualKeyCode::Minus) => {
+                                if brush_size > 1 {
+                                    brush_size -= 1
+                                }
+                            }
+                            Some(VirtualKeyCode::C) => sandbox = Sandbox::new(),
+                            Some(VirtualKeyCode::D) => {
+                                selected_particle = None;
+                            }
+                            Some(VirtualKeyCode::S) => {
+                                selected_particle = Some(ParticleType::Sand);
+                            }
+                            Some(VirtualKeyCode::W) => {
+                                selected_particle = Some(ParticleType::Water);
+                            }
+                            Some(VirtualKeyCode::A) => {
+                                selected_particle = Some(ParticleType::Acid);
+                            }
+                            Some(VirtualKeyCode::I) => {
+                                selected_particle = Some(ParticleType::Iridium);
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            },
+
+            Event::MainEventsCleared => {
+                if should_place_particles {
+                    let p1x = clamp(prev_cursor_position.x, 0.0, (DISPLAY_WIDTH - 2) as f64) / 2.0;
+                    let p1y = clamp(prev_cursor_position.y, 0.0, (DISPLAY_WIDTH - 2) as f64) / 2.0;
+                    let p2x = clamp(curr_cursor_position.x, 0.0, (DISPLAY_WIDTH - 2) as f64) / 2.0;
+                    let p2y = clamp(curr_cursor_position.y, 0.0, (DISPLAY_WIDTH - 2) as f64) / 2.0;
+                    let n = (p1x - p2y).abs().max((p1y - p2y).abs()) as usize;
+                    for step in 0..(n + 1) {
+                        let t = if n == 0 { 0.0 } else { step as f64 / n as f64 };
+                        let x = (p1x + t * (p2x - p1x)).round() as usize;
+                        let y = (p1y + t * (p2y - p1y)).round() as usize;
+                        for x in x..(x + brush_size) {
+                            for y in y..(y + brush_size) {
+                                if x < SIMULATION_WIDTH && y < SIMULATION_HEIGHT {
+                                    match selected_particle {
+                                        Some(selected_particle) => {
+                                            if sandbox.cells[x][y].is_none() {
+                                                sandbox.cells[x][y] =
+                                                    Some(Particle::new(selected_particle));
+                                            }
+                                        }
+                                        None => sandbox.cells[x][y] = None,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if last_update.elapsed() >= TARGET_TIME_PER_UPDATE && !paused {
+                    last_update = Instant::now();
+                    sandbox.update();
+                }
+
+                window.request_redraw();
+            }
+
+            Event::RedrawRequested(_) => {
+                sandbox.render(pixels.get_frame());
+                pixels.render().unwrap();
+            }
+            _ => {}
+        }
+    });
+}
+
+// TODO: Replace with the std version once rust adds it
+fn clamp(value: f64, min: f64, max: f64) -> f64 {
+    assert!(min <= max);
+    let mut x = value;
+    if x < min {
+        x = min;
+    }
+    if x > max {
+        x = max;
+    }
+    x
+}
---
+++ src/sandbox.rs
@@ -0,0 +1,247 @@
+use crate::{SIMULATION_HEIGHT, SIMULATION_WIDTH};
+use rand::rngs::ThreadRng;
+use rand::{thread_rng, Rng};
+
+pub struct Sandbox {
+    pub cells: [[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH],
+    rng: ThreadRng,
+}
+
+impl Sandbox {
+    pub fn new() -> Self {
+        Self {
+            cells: [[None; SIMULATION_HEIGHT]; SIMULATION_WIDTH],
+            rng: thread_rng(),
+        }
+    }
+
+    pub fn update(&mut self) {
+        for x in 0..SIMULATION_WIDTH {
+            for y in 0..SIMULATION_HEIGHT {
+                if let Some(particle) = &mut self.cells[x][y] {
+                    particle.updated = false;
+                }
+            }
+        }
+
+        for x in (0..SIMULATION_WIDTH).rev() {
+            for y in (0..SIMULATION_HEIGHT).rev() {
+                if let Some(particle) = &self.cells[x][y] {
+                    if !particle.updated {
+                        let mut new_particle_position = Some((x, y));
+                        loop {
+                            match particle.ptype {
+                                ParticleType::Sand => {
+                                    if y != SIMULATION_HEIGHT - 1 {
+                                        // Move 1 down if able
+                                        if self.cells[x][y + 1].is_none() {
+                                            self.cells[x][y + 1] = self.cells[x][y].take();
+                                            new_particle_position = Some((x, y + 1));
+                                            break;
+                                        }
+                                        // Else move 1 down and left if able
+                                        if x != 0 {
+                                            if self.cells[x - 1][y + 1].is_none() {
+                                                self.cells[x - 1][y + 1] = self.cells[x][y].take();
+                                                new_particle_position = Some((x - 1, y + 1));
+                                                break;
+                                            }
+                                        }
+                                        // Else move 1 down and right if able
+                                        if x != SIMULATION_WIDTH - 1 {
+                                            if self.cells[x + 1][y + 1].is_none() {
+                                                self.cells[x + 1][y + 1] = self.cells[x][y].take();
+                                                new_particle_position = Some((x + 1, y + 1));
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                ParticleType::Water | ParticleType::Acid => {
+                                    if y != SIMULATION_HEIGHT - 1 {
+                                        // Move 1 down if able
+                                        if self.cells[x][y + 1].is_none() {
+                                            self.cells[x][y + 1] = self.cells[x][y].take();
+                                            new_particle_position = Some((x, y + 1));
+                                            break;
+                                        }
+                                        // Else move 1 down and left if able
+                                        if x != 0 {
+                                            if self.cells[x - 1][y + 1].is_none() {
+                                                self.cells[x - 1][y + 1] = self.cells[x][y].take();
+                                                new_particle_position = Some((x - 1, y + 1));
+                                                break;
+                                            }
+                                        }
+                                        // Else move 1 down and right if able
+                                        if x != SIMULATION_WIDTH - 1 {
+                                            if self.cells[x + 1][y + 1].is_none() {
+                                                self.cells[x + 1][y + 1] = self.cells[x][y].take();
+                                                new_particle_position = Some((x + 1, y + 1));
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    // Else move left if able and a particle exists below, or if at the bottom of the screen
+                                    if x != 0 {
+                                        if self.cells[x - 1][y].is_none()
+                                            && if y == SIMULATION_HEIGHT - 1 {
+                                                true
+                                            } else {
+                                                self.cells[x - 1][y + 1].is_some()
+                                            }
+                                        {
+                                            self.cells[x - 1][y] = self.cells[x][y].take();
+                                            new_particle_position = Some((x - 1, y));
+                                            break;
+                                        }
+                                    }
+                                    // Else move right if able and a particle exists below, or if at the bottom of the screen
+                                    if x != SIMULATION_WIDTH - 1 {
+                                        if self.cells[x + 1][y].is_none()
+                                            && if y == SIMULATION_HEIGHT - 1 {
+                                                true
+                                            } else {
+                                                self.cells[x + 1][y + 1].is_some()
+                                            }
+                                        {
+                                            self.cells[x + 1][y] = self.cells[x][y].take();
+                                            new_particle_position = Some((x + 1, y));
+                                            break;
+                                        }
+                                    }
+                                }
+                                ParticleType::Iridium => {}
+                            }
+                            break;
+                        }
+                        if let Some((x, y)) = new_particle_position {
+                            self.cells[x][y].as_mut().unwrap().updated = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        for x in (0..SIMULATION_WIDTH).rev() {
+            for y in (0..SIMULATION_HEIGHT).rev() {
+                if let Some(particle1) = &self.cells[x][y] {
+                    if particle1.ptype != ParticleType::Acid
+                        && particle1.ptype != ParticleType::Iridium
+                    {
+                        loop {
+                            if y != SIMULATION_HEIGHT - 1 {
+                                if let Some(particle2) = self.cells[x][y + 1] {
+                                    if particle2.ptype == ParticleType::Acid {
+                                        self.cells[x][y] = None;
+                                        self.cells[x][y + 1] = None;
+                                        break;
+                                    }
+                                }
+                            }
+                            if x != SIMULATION_WIDTH - 1 {
+                                if let Some(particle2) = self.cells[x + 1][y] {
+                                    if particle2.ptype == ParticleType::Acid {
+                                        self.cells[x][y] = None;
+                                        self.cells[x + 1][y] = None;
+                                        break;
+                                    }
+                                }
+                            }
+                            if y != 0 {
+                                if let Some(particle2) = self.cells[x][y - 1] {
+                                    if particle2.ptype == ParticleType::Acid {
+                                        self.cells[x][y] = None;
+                                        self.cells[x][y - 1] = None;
+                                        break;
+                                    }
+                                }
+                            }
+                            if x != 0 {
+                                if let Some(particle2) = self.cells[x - 1][y] {
+                                    if particle2.ptype == ParticleType::Acid {
+                                        self.cells[x][y] = None;
+                                        self.cells[x - 1][y] = None;
+                                        break;
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn render(&mut self, frame: &mut [u8]) {
+        let mut i = 0;
+        for y in 0..SIMULATION_HEIGHT {
+            for x in 0..SIMULATION_WIDTH {
+                let mut color = (20, 20, 20);
+                if let Some(particle) = &self.cells[x][y] {
+                    color = match particle.ptype {
+                        ParticleType::Sand => (196, 192, 135),
+                        ParticleType::Water => (8, 130, 201),
+                        ParticleType::Acid => (128, 209, 0),
+                        ParticleType::Iridium => (205, 210, 211),
+                    };
+                    let noise = match particle.ptype {
+                        ParticleType::Water => 30,
+                        ParticleType::Acid => 50,
+                        _ => 0,
+                    };
+                    if noise != 0 {
+                        let m = self.rng.gen_range(-noise, noise + 1);
+                        color.0 = clamp(color.0 as i16 + m, 0, 255) as u8;
+                        color.1 = clamp(color.1 as i16 + m, 0, 255) as u8;
+                        color.2 = clamp(color.2 as i16 + m, 0, 255) as u8;
+                    }
+                }
+
+                frame[i] = color.0;
+                frame[i + 1] = color.1;
+                frame[i + 2] = color.2;
+                frame[i + 3] = 255;
+
+                i += 4;
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Particle {
+    pub ptype: ParticleType,
+    pub updated: bool,
+}
+
+impl Particle {
+    pub fn new(ptype: ParticleType) -> Self {
+        Self {
+            ptype,
+            updated: false,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum ParticleType {
+    Sand,
+    Water,
+    Acid,
+    Iridium,
+}
+
+// TODO: Replace with the std version once rust adds it
+fn clamp(value: i16, min: i16, max: i16) -> i16 {
+    assert!(min <= max);
+    let mut x = value;
+    if x < min {
+        x = min;
+    }
+    if x > max {
+        x = max;
+    }
+    x
+}
