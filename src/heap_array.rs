d2902dc39d84169ecf29966024276a6155f944a6
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-06-24 12:42:55 -0400

    Improve heap_array.rs

--- src/heap_array.rs
+++ src/heap_array.rs
@@ -6,17 +6,10 @@ pub fn create_cells_array(
 ) -> Box<[[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH]> {
     // Create a Vec of the same capacity as the resulting Box<Array>
     // We will never drop() this vec, as that would free the memory
-    let mut array: ManuallyDrop<Vec<Option<Particle>>> =
-        ManuallyDrop::new(Vec::with_capacity(SIMULATION_HEIGHT * SIMULATION_WIDTH));
-
-    // Fill the memory with the initial data
-    let ptr = array.as_mut_ptr();
-    for i in 0..(SIMULATION_HEIGHT * SIMULATION_WIDTH) {
-        unsafe { std::ptr::write::<Option<Particle>>(ptr.offset(i as isize), initial_value) };
-    }
-
+    let mut array = ManuallyDrop::new(vec![initial_value; SIMULATION_HEIGHT * SIMULATION_WIDTH]);
     // Convert the Vec to a Box<Array>
     // The box now owns the memory and is in charge of freeing it
+    let ptr = array.as_mut_ptr();
     unsafe { Box::from_raw(ptr as *mut [[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH]) }
 }
 
@@ -25,16 +18,12 @@ pub fn create_background_array(
 ) -> Box<[u8; SIMULATION_HEIGHT * SIMULATION_WIDTH * 4]> {
     // Create a Vec of the same capacity as the resulting Box<Array>
     // We will never drop() this vec, as that would free the memory
-    let mut array: ManuallyDrop<Vec<u8>> =
-        ManuallyDrop::new(Vec::with_capacity(SIMULATION_HEIGHT * SIMULATION_WIDTH * 4));
-
-    // Fill the memory with the initial data
-    let ptr = array.as_mut_ptr();
-    for i in 0..(SIMULATION_HEIGHT * SIMULATION_WIDTH * 4) {
-        unsafe { std::ptr::write::<u8>(ptr.offset(i as isize), initial_value) };
-    }
-
+    let mut array = ManuallyDrop::new(vec![
+        initial_value;
+        SIMULATION_HEIGHT * SIMULATION_WIDTH * 4
+    ]);
     // Convert the Vec to a Box<Array>
     // The box now owns the memory and is in charge of freeing it
+    let ptr = array.as_mut_ptr();
     unsafe { Box::from_raw(ptr as *mut [u8; SIMULATION_HEIGHT * SIMULATION_WIDTH * 4]) }
 }
