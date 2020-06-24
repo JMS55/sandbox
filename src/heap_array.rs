d05df10cebcb64e129afb6408da4fd7e91b784f3
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-06-24 12:54:57 -0400

    Cleanup heap_array.rs

--- src/heap_array.rs
+++ src/heap_array.rs
@@ -1,29 +1,30 @@
 use crate::sandbox::{Particle, SIMULATION_HEIGHT, SIMULATION_WIDTH};
 use std::mem::ManuallyDrop;
 
+// These functions create a heap allocated array like Box::new([T; N]), but unlike Box::new() the no data touches the stack
+// 1. Allocate a Vec of data. The Vec's buffer has the same memory representation as an equivalent array.
+// 2. Use unsafe magic to reinterpret the Vec's buffer as an array, and create a Box pointing to it. The Box now owns the memory.
+// *  Note: The Vec has to be wrapped with ManuallyDrop, else at the end of the function the Vec will be dropped, and the memory will cleaned up while the Box still points to it.
+
 pub fn create_cells_array(
     initial_value: Option<Particle>,
 ) -> Box<[[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH]> {
-    // Create a Vec of the same capacity as the resulting Box<Array>
-    // We will never drop() this vec, as that would free the memory
     let mut array = ManuallyDrop::new(vec![initial_value; SIMULATION_HEIGHT * SIMULATION_WIDTH]);
-    // Convert the Vec to a Box<Array>
-    // The box now owns the memory and is in charge of freeing it
-    let ptr = array.as_mut_ptr();
-    unsafe { Box::from_raw(ptr as *mut [[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH]) }
+    unsafe {
+        Box::from_raw(
+            array.as_mut_ptr() as *mut [[Option<Particle>; SIMULATION_HEIGHT]; SIMULATION_WIDTH]
+        )
+    }
 }
 
 pub fn create_background_array(
     initial_value: u8,
 ) -> Box<[u8; SIMULATION_HEIGHT * SIMULATION_WIDTH * 4]> {
-    // Create a Vec of the same capacity as the resulting Box<Array>
-    // We will never drop() this vec, as that would free the memory
     let mut array = ManuallyDrop::new(vec![
         initial_value;
         SIMULATION_HEIGHT * SIMULATION_WIDTH * 4
     ]);
-    // Convert the Vec to a Box<Array>
-    // The box now owns the memory and is in charge of freeing it
-    let ptr = array.as_mut_ptr();
-    unsafe { Box::from_raw(ptr as *mut [u8; SIMULATION_HEIGHT * SIMULATION_WIDTH * 4]) }
+    unsafe {
+        Box::from_raw(array.as_mut_ptr() as *mut [u8; SIMULATION_HEIGHT * SIMULATION_WIDTH * 4])
+    }
 }
